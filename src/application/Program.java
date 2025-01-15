d0d656ba0d7f0a49afa9cca88d2cfc0732b1d2d5
Pedro Almeida <pedro.almeida@rtc.pt>
2025-01-15 14:39:29 +0000

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -33,9 +33,15 @@ public class Program {
 		}
 		
 		System.out.println("\n === TEST 4: seller insert ===");
-		Seller newSeller = new Seller (null, "Greg", "greg@gmail.com", new Date(),  4000.00, department);
+		Seller newSeller = new Seller (null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n === TEST 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	}
 
 }
